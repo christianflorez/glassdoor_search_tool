9371d04f052a3c7dfc6de879e9bfe702f1660784
Christian Florez <florez.christian@gmail.com>
2017-05-08 15:08:25 -0400

    add comments

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,8 @@ router.get('/', function(req, res, next) {
   if (JSON.stringify(req.query) === "{}"){
     res.render('index');
   } else {
+
+    // set up necessary variables for search
     let pageNumber = req.query.pageNumber || 1;
     let nextPage = parseInt(pageNumber) + 1;
     let prevPage = pageNumber === 1 ? 1 : parseInt(pageNumber) - 1;
@@ -22,8 +24,12 @@ router.get('/', function(req, res, next) {
     let minReviews = parseFloat(req.query.minReviews) || REVIEWS_DEFAULT;
     let glassdoor = new Glassdoor(userIP, useragent);
 
+    // queries glassdoor api with search terms
     glassdoor.getEmployersByZip(zipcode, pageNumber, keywords, (data) => {
       results = [];
+
+      // filters by user provided minimum ratings & reviews
+      // note: glassdoor API does not offer native filtering
       data.employers.forEach((employer) => {
         if (employer.numberOfRatings >= minRatings && employer.overallRating >= minReviews) {
           results.push(employer);
@@ -35,6 +41,7 @@ router.get('/', function(req, res, next) {
         prevPage: prevPage,
         zipcode: zipcode,
         employers: results.length > 0 ? results : [
+          // provides default values when there are no results on the page
           {"error": "Nothing found on this page!", 
           "overallRating": "N/A", 
           "numberOfRatings": "N/A", 
